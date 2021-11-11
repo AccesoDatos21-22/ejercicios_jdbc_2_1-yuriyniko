f9590059e5c1b904893ddcbc8816abe5eef9da87
YuriyBTK <yuriybutko.smr@gmail.com>
2021-11-11 19:17:47 +0100

    Ejercicio_6_y_7 terminado

--- Main.java
+++ Main.java
@@ -1,27 +1,73 @@
 import org.iesinfantaelena.dao.Cafes;
 import org.iesinfantaelena.dao.Libros;
 import org.iesinfantaelena.modelo.AccesoDatosException;
+import org.iesinfantaelena.modelo.Libro;
 
 public class Main {
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
 
-	
-		try {
-			Cafes cafes = new Cafes();
-      cafes.insertar("Cafetito", 150, 1.0f, 100,1000);
-      cafes.insertar("Cafe tacilla", 150, 2.0f, 100,1000);
-      cafes.verTabla();
-      cafes.buscar("tacilla");
-      cafes.cafesPorProveedor(150);
-      cafes.borrar("Cafe tacilla");
-      cafes.verTabla();
 
-      
-		
-		} catch (AccesoDatosException e) {
-			e.printStackTrace();
-		}
-	}
+        try {
+            Cafes cafes = new Cafes();
+            cafes.insertar("Cafetito", 150, 1.0f, 100, 1000);
+            cafes.insertar("tacilla", 49, 2.0f, 101, 1001);
+            cafes.verTabla();
+            System.out.println();
+            cafes.buscar("tacilla");
+            System.out.println();
+            cafes.cafesPorProveedor(49);
+            System.out.println();
+            cafes.borrar("tacilla");
+            cafes.verTabla();
+
+            System.out.println("Cerramos cafes");
+            cafes.cerrar();
+
+            Libros libros = new Libros();
+
+            Libro l1 = new Libro(12345, "Sistemas Operativos", "Tanembaun", "Informatica", 156, 3);
+            Libro l2 = new Libro(12453, "Minix", "Stallings", "Informatica", 345, 4);
+            Libro l3 = new Libro(1325, "Linux", "Richard Stallman", "FSF", 168, 10);
+            Libro l4 = new Libro(1725, "Java", "Juan Garcia", "Programacion", 245, 9);
+
+            libros.anadirLibro(l1);
+            libros.anadirLibro(l2);
+            libros.anadirLibro(l3);
+            libros.anadirLibro(l4);
+
+            System.out.println("Lista libros");
+
+            libros.verCatalogo();
+
+            System.out.println("Campos lobro");
+
+            String[] campos = libros.getCamposLibro();
+
+            for (String camp : campos) {
+                System.out.println(camp);
+            }
+
+
+            System.out.println("Libros por isbn");
+            libros.obtenerLibro(1325);
+            System.out.println("Actualizamos paginas");
+            libros.actualizarCopias(l2);
+            System.out.println("Borramos");
+            libros.borrar(l1);
+
+            System.out.println("Listamos");
+
+            libros.verCatalogo();
+
+            System.out.println("Cerramos libros");
+
+            libros.cerrar();
+
+
+        } catch (AccesoDatosException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
--- org.iesinfantaelena.dao/Libros.java
+++ org.iesinfantaelena.dao/Libros.java
@@ -21,10 +21,16 @@ public class Libros {
 	private Statement stmt;
 	private ResultSet rs;
 	private PreparedStatement pstmt;
+	private ResultSetMetaData rsmd;
 
 	// Consultas a realizar en BD
 
 	private static final String INSERT_LIBROS_QUERY = "insert into libros values (?,?,?,?,?,?)";
+	private static final String SELECT_LIBROS_QUERY = "select * from libros";
+	private static final String DELETE_LIBROS_QUERY = "delete from libros WHERE isbn = ?";
+	private static final String UPDATE_LIBROS_QUERY = "update libros set copias=? WHERE isbn = ?";
+	private static final String SEARCH_LIBROS_QUERY = "SELECT * from libros WHERE isbn = ?";
+	private static final String SELECT_CAMPOS_QUERY = "SELECT * FROM libros LIMIT 1";
 
 	private static final String CREATE_TABLE_LIBROS = "create table if not exists libros (isbn integer not null,titulo varchar(50) not null,autor varchar(50) not null,editorial varchar(25) not null,paginas integer not null,copias integer not null,constraint isbn_pk primary key (isbn));";
 
@@ -104,9 +110,30 @@ public class Libros {
 	 */
 	
 	public List<Libro> verCatalogo() throws AccesoDatosException {
-	
-		return null;
+		List<Libro> libros =new ArrayList<Libro>();
 
+		try {
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(SELECT_LIBROS_QUERY);
+
+			while (rs.next()) {
+				Libro temp = new Libro();
+				temp.setISBN(rs.getInt("isbn"));
+				temp.setTitulo(rs.getString("titulo"));
+				temp.setAutor(rs.getString("autor"));
+				temp.setEditorial(rs.getString("editorial"));
+				temp.setPaginas(rs.getInt("paginas"));
+				temp.setCopias(rs.getInt("copias"));
+
+				libros.add(temp);
+			}
+		} catch (SQLException sqle) {
+			Utilidades.printSQLException(sqle);
+			throw new AccesoDatosException("Ocurrió un error al acceder a los datos");
+		} finally {
+			liberar();
+		}
+		return libros;
 	}
 
     /**
@@ -116,7 +143,22 @@ public class Libros {
      */
 	
 	public void actualizarCopias(Libro libro) throws AccesoDatosException {
-		
+		try {
+			int numpag=100;
+
+			pstmt = con.prepareStatement(UPDATE_LIBROS_QUERY);
+			pstmt.setInt(2, libro.getISBN());
+			pstmt.setInt(1, numpag);
+
+			pstmt.executeUpdate();
+			System.out.println("Libro" + libro.getISBN() + " ha .sido modificado");
+
+		} catch (SQLException sqle) {
+			Utilidades.printSQLException(sqle);
+			throw new AccesoDatosException("Ocurrió un error al acceder a los datos");
+		} finally {
+			liberar();
+		}
 	}
 
     /**
@@ -152,8 +194,18 @@ public class Libros {
 	 */
 
 	public void borrar(Libro libro) throws AccesoDatosException {
-		
-		
+		try {
+			pstmt = con.prepareStatement(DELETE_LIBROS_QUERY);
+			pstmt.setInt(1, libro.getISBN());
+
+			pstmt.executeUpdate();
+			System.out.println("Libro" + libro.getISBN() + " ha sido borrado.");
+		} catch (SQLException sqle) {
+			Utilidades.printSQLException(sqle);
+			throw new AccesoDatosException("Ocurrió un error al acceder a los datos");
+		} finally {
+			liberar();
+		}
 	}
 	
 	/**
@@ -163,12 +215,64 @@ public class Libros {
 	 */
 
 	public String[] getCamposLibro() throws AccesoDatosException {
-       
-    return null;
+		rsmd = null;
+		String[] campos = null;
+
+		try {
+			pstmt = con.prepareStatement(SELECT_CAMPOS_QUERY);
+
+			rs = pstmt.executeQuery();
+			rsmd = rs.getMetaData();
+
+			int columns = rsmd.getColumnCount();
+
+			campos = new String[columns];
+
+			for (int i = 0; i < columns; i++) {
+				campos[i] = rsmd.getColumnLabel(i + 1);
+			}
+			return campos;
+
+		} catch (SQLException sqle) {
+			// En una aplicación real, escribo en el log y delego
+			Utilidades.printSQLException(sqle);
+			throw new AccesoDatosException("Ocurrió un error al acceder a los datos");
+		} finally {
+			liberar();
+		}
 	}
 
+	/**
+	 * Devulve los libros por isbn
+	 *
+	 * @throws AccesoDatosException
+	 */
+
 	public void obtenerLibro(int ISBN) throws AccesoDatosException {
-		
+		try {
+			pstmt = con.prepareStatement(SEARCH_LIBROS_QUERY);
+			pstmt.setInt(1, ISBN);
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+
+				int isbn = rs.getInt("isbn");
+				String titulo = rs.getString("titulo");
+				String autor = rs.getString("autor");
+				String editorial = rs.getString("editorial");
+				int paginas = rs.getInt("paginas");
+				int copias = rs.getInt("copias");
+
+				System.out.println(isbn + ", " + titulo + ", "
+						+ autor + ", " + editorial + ", " + paginas + ", " + copias);
+			}
+		} catch (SQLException sqle) {
+			Utilidades.printSQLException(sqle);
+			throw new AccesoDatosException("Ocurrió un error al acceder a los datos");
+		} finally {
+			liberar();
+		}
 	}
 
 }
